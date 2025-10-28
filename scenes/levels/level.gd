7008a2f64f7d4aadb7ef1747bc38b3c9cd7c6be2
Yvan Pangilinan <yvanpdev5@gmail.com>
2025-10-27 23:37:15 -0400

    Added laser and grenade logic
    Grenade WIP still with physics

--- scenes/levels/level.gd
+++ scenes/levels/level.gd
@@ -1,14 +1,19 @@
 extends Node2D
 
+var laser_scene: PackedScene = preload("res://scenes/projectiles/laser.tscn")
+var grenade_scene: PackedScene = preload("res://scenes/projectiles/grenade.tscn")
 
 func _on_gate_player_entered_gate(body) -> void:
 	print("player has entered gate")
 	print(body)
 
 
-func _on_player_laser() -> void:
-	print("laser from level")
-
-
-func _on_player_grenade() -> void:
-	print("grenade from level")
+func _on_player_laser(pos) -> void:
+	var laser = laser_scene.instantiate()
+	laser.position = pos
+	$Projectiles.add_child(laser)
+	
+func _on_player_grenade(pos) -> void:
+	var grenade = grenade_scene.instantiate()
+	grenade.position = pos
+	$Projectiles.add_child(grenade)
--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -3,8 +3,8 @@ extends CharacterBody2D
 var can_laser: bool = true
 var can_grenade: bool = true
 
-signal laser
-signal grenade
+signal laser(pos)
+signal grenade(pos)
 
 func _process(_delta: float) -> void:
 	
@@ -14,16 +14,18 @@ func _process(_delta: float) -> void:
 	
 	# laser shooting input
 	if Input.is_action_pressed("primary action") and can_laser:
-		print("shoot laser")
+		var laser_markers = $LaserStartPositions.get_children()
+		var selected_laser = laser_markers[randi() % laser_markers.size()]
 		can_laser = false
 		$LaserReloadTimer.start()
-		laser.emit()
+		laser.emit(selected_laser.global_position)
 		
 	if Input.is_action_pressed("secondary action") and can_grenade:
-		print("shoot grenade")
+		var grenade_markers = $LaserStartPositions.get_children()
+		var selected_grenade = grenade_markers[randi() % grenade_markers.size()]
 		can_grenade = false
 		$GrenadeReloadTimer.start()
-		grenade.emit()
+		grenade.emit(selected_grenade.global_position)
 		
 func _on_laser_reload_timer_timeout() -> void:
 	can_laser = true
